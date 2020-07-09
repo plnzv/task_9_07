39d51320ae369ac22689879617d8cec3fbeced82
Alestr <user201503@gmail.com>
2020-07-09 20:46:15 +0800

    new JS

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,15 @@
-let money = prompt("Ваш бюджет на месяц?")
-let time = prompt("Введите дату в формате YYYY-MM-DD")
-appData = {}
-appData.money = money
-console.log(appData[money])
+"use strict"
+
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {};
+appData.money = money;
+appData.timeData = time;
+let Expen = prompt("Введите обязательную статью расходов в этом месяце");
+let optExpensens = prompt("Во сколько обойдется?")  ;
+let Expensens = {};
+Expensens.Expensens = Expen.split(", ");
+Expensens.optionalExpenses = optExpensens.split(", ");
+appData.Expensens = Expensens.Expensens;
+appData.optionalExpenses = Expensens.optionalExpenses;
+console.log(appData);
